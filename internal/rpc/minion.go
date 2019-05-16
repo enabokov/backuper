ed9f29a6f70eb3c9edd7c9c43932f47ef778110a
enabokov <edwardnabokov@gmail.com>
2019-05-16 16:32:29 +0300

    Rewrite rpc to be used by many dbs

--- internal/rpc/minion.go
+++ internal/rpc/minion.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strconv"
 
 	"github.com/enabokov/backuper/internal/config"
 	"github.com/enabokov/backuper/internal/log"
@@ -23,14 +24,32 @@ func init() {
 func (m *Minion) GetNamespaces(ctx context.Context, query *minion.Query) (*minion.Namespaces, error) {
 	log.Info.Println(query)
 
+	var (
+		dirNames []string
+		dirSizes []string
+		ok       []float64
+	)
+
 	conf := c.GetMinionConf()
-	src := hbase.SourceFile{
-		HostNameNode: fmt.Sprintf("%s:%d", conf.NameNode.Host, conf.NameNode.Port),
-		Filename:     query.Query,
+
+	switch query.Db {
+	case `hbase`:
+		sock := hbase.Socket{
+			IP:   conf.NameNode.Host,
+			Port: strconv.Itoa(conf.NameNode.Port),
+		}
+
+		dirNames, dirSizes, ok = hbase.GetNamespaces(sock)
+		break
+	case `postgres`:
+		break
+	case `mongodb`:
+		break
+	case `cassandra`:
+		break
 	}
 
-	dirNames, dirSizes := hbase.GetNamespaces(src)
-	return &minion.Namespaces{Names: dirNames, Sizes: dirSizes}, nil
+	return &minion.Namespaces{Names: dirNames, Sizes: dirSizes, Ok: ok}, nil
 }
 
 func (m *Minion) GetTables(ctx context.Context, query *minion.Query) (*minion.Tables, error) {
@@ -40,21 +59,43 @@ func (m *Minion) GetTables(ctx context.Context, query *minion.Query) (*minion.Ta
 func (m *Minion) StartBackup(ctx context.Context, query *minion.Query) (*minion.Response, error) {
 	log.Info.Println(query)
 
+	var (
+		msg string
+	)
+
 	conf := c.GetMinionConf()
 
-	src := hbase.SourceFile{
-		HostNameNode: fmt.Sprintf("%s:%d", conf.NameNode.Host, conf.NameNode.Port),
-		Filename:     query.Query,
+	switch query.Db {
+	case `hbase`:
+		sock := hbase.Socket{
+			IP:   conf.NameNode.Host,
+			Port: strconv.Itoa(conf.NameNode.Port),
+		}
+
+		// TODO: add check in case sql injections
+		srcFilename := query.Query
+
+		dst := hbase.S3Options{
+			Region:     conf.Targets.S3.Bucket.Region,
+			BucketName: conf.Targets.S3.Bucket.Name,
+			Key:        filepath.Join(`backup_hbase`, service.GetPrivateIP()),
+		}
+
+		go hbase.CopyToS3Bucket(sock, srcFilename, dst)
+		msg = fmt.Sprintf("Backup %s -> %s", srcFilename, filepath.Join(dst.BucketName, dst.Key))
+		break
+	case `postgres`:
+		msg = "Not implemented yet"
+		break
+	case `mongodb`:
+		msg = "Not implemented yet"
+		break
+	case `cassandra`:
+		msg = "Not implemented yet"
+		break
 	}
 
-	dst := hbase.TargetS3{
-		Region:     conf.Targets.S3.Bucket.Region,
-		BucketName: conf.Targets.S3.Bucket.Name,
-		Key:        filepath.Join(`backup_hbase`, service.GetPrivateIP()),
-	}
-
-	go hbase.CopyToS3Bucket(src, dst)
-	return &minion.Response{Msg: query.Query + " backup started"}, nil
+	return &minion.Response{Msg: msg}, nil
 }
 
 func (m *Minion) StopBackup(ctx context.Context, query *minion.Query) (*minion.Response, error) {
